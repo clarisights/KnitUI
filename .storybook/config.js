b1aff542a97dcb6ea6aa88fa486866b901f74533
Rohan Malhotra <rohan.root@gmail.com>
2019-03-08 15:49:17 +0530

    changes stories format

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from "@storybook/react"
 // automatically import all files ending in *.stories.tsx
-const req = require.context("../stories", true, /.stories.tsx$/)
+const req = require.context("../components", true, /.stories.tsx$/)
 
 function loadStories() {
   req.keys().forEach(req)
