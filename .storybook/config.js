e502c533119d2f087571a170cdcc49862eb479b5
Rohan Malhotra <rohan.root@gmail.com>
2019-03-09 18:25:06 +0530

    added global decorator for addInfo

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
-import { configure } from "@storybook/react"
+import { configure, addDecorator } from "@storybook/react"
+import { withInfo } from "@storybook/addon-info"
 // automatically import all files ending in *.stories.tsx
 const req = require.context("../components", true, /.stories.tsx$/)
 
 function loadStories() {
   req.keys().forEach(req)
 }
+
+addDecorator(
+  withInfo({
+    inline: true,
+    header: false,
+  })
+)
 
 configure(loadStories, module)
