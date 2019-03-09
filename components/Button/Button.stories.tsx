d2af02ec5e6392ae39c2b5995fb4eacdc6ef3792
Rohan Malhotra <rohan.root@gmail.com>
2019-03-09 18:32:34 +0530

    added knobs & actions storybook addon

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,7 +1,9 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
+import { withActions } from "@storybook/addon-actions"
 import { Button } from "../../components"
 
 storiesOf("Button", module)
+  .addDecorator(withActions("mouseover", "click .btn"))
   .add("with text", () => <Button name="test" />)
   .add("with some emoji", () => <Button name="😀 😎 👍 💯" />)
