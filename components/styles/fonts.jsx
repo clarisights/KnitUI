acbe8ff7fc94d68569f9df3cfaa8c64f3461a920
arorayash <arorayash.thisisus@gmail.com>
2019-03-10 09:07:01 +0530

    added fonts

---
+++ components/styles/fonts.jsx
@@ -0,0 +1,15 @@
+import InterThin from "../../../assets/fonts/Inter-Thin.woff";
+import InterLight from "../../../assets/fonts/Inter-Light.woff"
+import InterRegular from "../../../assets/fonts/Inter-Regular.woff"
+import InterMedium from "../../../assets/fonts/Inter-Medium.woff"
+import InterBold from "../../../assets/fonts/Inter-Bold.woff"
+import InterBlack from "../../../assets/fonts/Inter-Black.woff"
+
+export const {
+    InterThin,
+    InterLight,
+    InterRegular,
+    InterMedium,
+    InterBold,
+    InterBlack
+}
