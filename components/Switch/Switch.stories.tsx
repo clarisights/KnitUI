0a7d9125ebcbae39ef8177f826b30a6d16af8843
Rohan Malhotra <rohan.root@gmail.com>
2019-03-28 19:08:14 +0530

    Added switch component

---
+++ components/Switch/Switch.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import { Switch } from "./index"
+
+storiesOf("Switch", module)
+  .add("Basic switch", () => <Switch />)
+  .add("Small switch with default checked", () => (
+    <Switch size="small" defaultChecked />
+  ))
+  .add("Default Switch with disabled", () => <Switch disabled />)
+  .add("Default Switch with children", () => (
+    <Switch checkedChildren="1" unCheckedChildren="0" />
+  ))
+  .add("Small Switch with children", () => (
+    <Switch size="small" checkedChildren="1" unCheckedChildren="0" />
+  ))
