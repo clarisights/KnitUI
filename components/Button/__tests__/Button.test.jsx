6f2fe9fe428108b179acdfb28eea1da5adac8501
Kushagra Sharma <kushagra8888@gmail.com>
2019-05-03 16:24:37 +0530

    add basic snaphost tests

--- components/Button/__tests__/Button.test.jsx
+++ components/Button/__tests__/Button.test.jsx
@@ -3,8 +3,32 @@ import { Button } from "../index"
 import renderer from 'react-test-renderer';
 
 describe("Button", () => {
-  it("should render correctly", () => {
+  it("renders a primary button with a label correctly", () => {
     const tree = renderer.create(<Button label="button" />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it("renders a secondary button with a label correctly", () => {
+    const tree = renderer.create(<Button label="button" type="secondary" />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it("renders a ghost button correctly", () => {
+    const tree = renderer.create(<Button label="button" ghost/>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it("renders a button with inset correctly", () => {
+    const tree = renderer.create(<Button label="button" insetLabel={"10"}/>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it("renders a button with an icon correctly", () => {
+    const tree = renderer.create(<Button label="button" icon="Info"/>).toJSON()
+    expect(tree).toMatchSnapshot()
+  }),
+  it("renders a button with text and an icon correctly", () => {
+    const tree = renderer.create(<Button label="button" icon="Info"/>).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+  it("renders a button with text, icon and inset correctly", () => {
+    const tree = renderer.create(<Button label="button" icon="Info" insetLabel={"10"}/>).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
