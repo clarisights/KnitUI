33f75fb7f19090badf3f7cf7d38df2ee865eb1b7
arorayash <arorayash.thisisus@gmail.com>
2019-05-14 11:10:03 +0530

    added hues as per Design lib

---
+++ components/styles/palette.tsx
@@ -0,0 +1,144 @@
+/* Primary Palette
+---------------
+The primary palette comprises bright and bold range of hues derived from Lapis lazuli. These should be used selectively to highlight the important actions and guide the eye.
+*/
+
+export const Blue100 = {
+  hsl: [216, 100, 20],
+  hex: "#002966",
+}
+
+export const Azure80 = {
+  hsl: [205, 85, 65],
+  hex: "#5AB2F2",
+}
+
+export const LightBlue40 = {
+  hsl: [203, 100, 95],
+  hex: "#E5F5FF",
+}
+
+export const Neutral0 = {
+  hsl: [0, 0, 100],
+  hex: "#FFFFFF",
+}
+
+/* Secondary Palette
+--------------------
+The secondary pallete comprises composed hues that compliment the primaries well. These provide a warmer tone to the product, and are deeply associated with the state and type of action, for instance, Crimson - danger/failure, Dark Lime - success, Pure Yellow - warning alerts, Moderate magenta - unsaved state.
+*/
+
+export const Crimson80 = {
+  hsl: [0, 100, 30],
+  hex: "#990000",
+}
+
+export const Green80 = {
+  hsl: [118, 100, 20],
+  hex: "#036600",
+}
+
+export const Yellow80 = {
+  hsl: [47, 100, 50],
+  hex: "#FFC700",
+}
+
+export const Magenta80 = {
+  hsl: [288, 20, 35],
+  hex: "#64476B",
+}
+
+/* Tertiary Palette
+---------------
+The tertiary palette comprises the washed-out hues that are used for highlights. It provides a softer experience and help in easily differentiating between bits.
+*/
+
+export const Beige10 = {
+  hsl: [42, 38, 95],
+  hex: "#F7F4ED",
+}
+
+export const Rose10 = {
+  hsl: [0, 100, 95],
+  hex: "#FFE3E3",
+}
+
+export const Fern10 = {
+  hsl: [148, 33, 90],
+  hex: "#DDEEE5",
+}
+
+export const Sun10 = {
+  hsl: [40, 92, 90],
+  hex: "#FDEDCE",
+}
+
+export const Sapphire10 = {
+  hsl: [217, 80, 90],
+  hex: "#D1E1FA",
+}
+
+export const Violet10 = {
+  hsl: [274, 49, 90],
+  hex: "#E7D9F2",
+}
+
+export const Coral10 = {
+  hsl: [22, 80, 90],
+  hex: "#FAE0D1",
+}
+
+export const Wood10 = {
+  hsl: [33, 35, 85],
+  hex: "#E6DACB",
+}
+
+/* Neutral Palette
+---------------
+The neutrals bring balance to the color palette, and used for actions that either don’t need direct attention or reduce focus from those already taking more than needed. Darker neutrals are used for shadows, borders, etc.
+*/
+
+export const Neutral10 = {
+  hsl: [0, 0, 95],
+  hex: "#F2F2F2",
+}
+
+export const Neutral20 = {
+  hsl: [0, 0, 90],
+  hex: "#E5E5E5",
+}
+
+export const Neutral30 = {
+  hsl: [0, 0, 85],
+  hex: "#D9D9D9",
+}
+
+export const Neutral40 = {
+  hsl: [0, 0, 80],
+  hex: "#CCCCCC",
+}
+
+export const Neutral50 = {
+  hsl: [0, 0, 50],
+  hex: "#808080",
+}
+
+export const Neutral60 = {
+  hsl: [0, 0, 40],
+  hex: "#666666",
+}
+
+export const Neutral70 = {
+  hsl: [0, 0, 30],
+  hex: "#4C4C4C",
+}
+
+export const Neutral80 = {
+  hsl: [0, 0, 20],
+  hex: "#333333",
+}
+
+export const Neutral90 = {
+  hsl: [0, 0, 10],
+  hex: "#1A1A1A",
+}
