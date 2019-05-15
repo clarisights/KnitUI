0fa39d470bc6d310d44e9e0686c05947e93d3f90
Kushagra Sharma <kushagra8888@gmail.com>
2019-05-15 15:23:28 +0530

    add tests

---
+++ components/Modal/__tests__/Modal.test.tsx
@@ -0,0 +1,131 @@
+import React from "react"
+import { Modal, RightPanelModal, LeftPanelModal, BottomPanelModal } from "../index"
+import { render, cleanup, fireEvent } from 'react-testing-library'
+import 'jest-styled-components'
+import 'jest-dom/extend-expect'
+import { ThemeProvider } from "../../styles"
+
+afterEach(cleanup)
+
+describe("Modal", () => {
+  const sizes = ["small", "medium", "large", "x-large"]
+  sizes.forEach((size) => {
+    describe(`with size ${size}`, () => {
+      it("with simple config", () => {
+        const { container } = render(
+          <ThemeProvider>
+            <Modal
+              header={{title: "Title"}}
+              body={<div>Body</div>}
+              footer={<div>Footer</div>}
+              size="medium"
+              visible
+              onClose={() => {}}
+            />
+          </ThemeProvider>
+        )
+        const dialogContainer = container.parentElement!.querySelector(".rc-dialog-wrap")
+        expect(dialogContainer).toMatchSnapshot()
+      })
+
+      it("with right section in header", () => {
+        const { container } = render(
+          <ThemeProvider>
+            <Modal
+              header={{
+                title: "Title",
+                rightSection:
+                  <div>
+                    <div>The right section</div>
+                  </div>
+              }}
+              body={<div>Body</div>}
+              footer={<div>Footer</div>}
+              size="medium"
+              visible
+              onClose={() => {}}
+            />
+          </ThemeProvider>
+        )
+        const dialogContainer = container.parentElement!.querySelector(".rc-dialog-wrap")
+        expect(dialogContainer).toMatchSnapshot()
+      })
+
+      it("with a right panel", () => {
+        const { container } = render(
+          <ThemeProvider>
+            <RightPanelModal
+              header={{title: "Title"}}
+              body={<div>Body</div>}
+              footer={<div>Footer</div>}
+              panelContent={<div>Pannel content</div>}
+              size="medium"
+              visible
+              onClose={() => {}}
+            />
+          </ThemeProvider>
+        )
+        const dialogContainer = container.parentElement!.querySelector(".rc-dialog-wrap")
+        expect(dialogContainer).toMatchSnapshot()
+      })
+
+      it("with a left panel", () => {
+        const { container } = render(
+          <ThemeProvider>
+            <LeftPanelModal
+              header={{title: "Title"}}
+              body={<div>Body</div>}
+              footer={<div>Footer</div>}
+              panelContent={<div>Pannel content</div>}
+              size="medium"
+              visible
+              onClose={() => {}}
+            />
+          </ThemeProvider>
+        )
+        const dialogContainer = container.parentElement!.querySelector(".rc-dialog-wrap")
+        expect(dialogContainer).toMatchSnapshot()
+      })
+
+      it("with a bottom panel", () => {
+        const { container } = render(
+          <ThemeProvider>
+            <BottomPanelModal
+              header={{title: "Title"}}
+              body={<div>Body</div>}
+              footer={<div>Footer</div>}
+              panelContent={<div>Pannel content</div>}
+              size="medium"
+              visible
+              onClose={() => {}}
+            />
+          </ThemeProvider>
+        )
+        const dialogContainer = container.parentElement!.querySelector(".rc-dialog-wrap")
+        expect(dialogContainer).toMatchSnapshot()
+      })
+    })
+  })
+
+  it("should call the onClose handler when the close button is clicked", () => {
+    const onCloseStub = jest.fn()
+    const { container } = render(
+      <ThemeProvider>
+        <BottomPanelModal
+          header={{title: "Title"}}
+          body={<div>Body</div>}
+          footer={<div>Footer</div>}
+          panelContent={<div>Pannel content</div>}
+          size="medium"
+          visible
+          onClose={onCloseStub}
+        />
+      </ThemeProvider>
+    )
+
+    const dialogContainer = container.parentElement!.querySelector(".rc-dialog-wrap")
+    const closeButton = dialogContainer!.querySelector(".rc-dialog-close")
+    fireEvent.click(closeButton!)
+    expect(onCloseStub).toBeCalled()
+  })
+})
---
+++ components/_utils/__mocks__/fileMock.ts
@@ -0,0 +1,5 @@
+// To enabel jest to run tests for components that import static
+// assets like images and fonts.
+// https://jestjs.io/docs/en/webpack#handling-static-assets
+
+module.exports = 'test-file-stub'
