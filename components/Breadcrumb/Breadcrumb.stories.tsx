0af405659ab8d9f564541790f9b32522c99c6efb
Anshuman Verma <anshu.av97@gmail.com>
2019-05-22 23:04:27 +0530

    fix stories, separator bug

--- components/Breadcrumb/Breadcrumb.stories.tsx
+++ components/Breadcrumb/Breadcrumb.stories.tsx
@@ -1,28 +1,27 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
-import Breadcrumb from './index';
-import BreadcrumbItem from './BreadcrumbItem';
+import Breadcrumb from './index'
 
 const sep = <span>🐉</span>;
 
 storiesOf("Breadcrumb", module)
   .add("Basic Breadcrumb", () => (
     <Breadcrumb>
-      <BreadcrumbItem>Dashboard</BreadcrumbItem>
-      <BreadcrumbItem>Reports</BreadcrumbItem>
-      <BreadcrumbItem >50467</BreadcrumbItem>
+      <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+      <Breadcrumb.Item>Reports</Breadcrumb.Item>
+      <Breadcrumb.Item >50467</Breadcrumb.Item>
     </Breadcrumb>
   ))
   .add("Breadcrumb with link", () => (
     <Breadcrumb>
-      <BreadcrumbItem>Dashboard</BreadcrumbItem>
-      <BreadcrumbItem>Reports</BreadcrumbItem>
-      <BreadcrumbItem href="/dashboard/reports/50467">50467</BreadcrumbItem>
+      <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+      <Breadcrumb.Item>Reports</Breadcrumb.Item>
+      <Breadcrumb.Item href="/dashboard/reports/50467">50467</Breadcrumb.Item>
     </Breadcrumb>
   )).add("Breadcrumb with separator", () => (
     <Breadcrumb separator={sep}>
-      <BreadcrumbItem>Dashboard</BreadcrumbItem>
-      <BreadcrumbItem>Reports</BreadcrumbItem>
-      <BreadcrumbItem href="/dashboard/reports/50467">50467</BreadcrumbItem>
+      <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+      <Breadcrumb.Item>Reports</Breadcrumb.Item>
+      <Breadcrumb.Item href="/dashboard/reports/50467">50467</Breadcrumb.Item>
   </Breadcrumb>
   ));
