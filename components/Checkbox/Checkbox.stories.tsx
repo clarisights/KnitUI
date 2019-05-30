a8377b39695ce277e1d133df16f67226a379fdaf
Anshuman Verma <anshu.av97@gmail.com>
2019-05-30 14:54:20 +0530

    forma

--- components/Checkbox/Checkbox.stories.tsx
+++ components/Checkbox/Checkbox.stories.tsx
@@ -4,18 +4,14 @@ import { Checkbox } from "./index"
 
 storiesOf("Checkbox", module)
   .add("Basic", () => <Checkbox />)
-  .add("Default with checked", () => (
-    <Checkbox defaultChecked />
-  ))
-  .add("Small with default checked", () => (
-    <Checkbox size="small" checked />
-  ))
-  .add("Checkbox with label", () => (
-    <Checkbox>Checkbox</Checkbox>
-  ))
+  .add("Default with checked", () => <Checkbox defaultChecked />)
+  .add("Small with default checked", () => <Checkbox size="small" checked />)
+  .add("Checkbox with label", () => <Checkbox>Checkbox</Checkbox>)
   .add("Default with disabled", () => <Checkbox disabled />)
-  .add("Default with disabled but checked", () => <Checkbox disabled checked/>)
-  .add("Default with label and disabled with checked", () => <Checkbox disabled checked>Checkbox Disabled</Checkbox>)
-  .add("Small with children", () => (
-    <Checkbox size="small" />
+  .add("Default with disabled but checked", () => <Checkbox disabled checked />)
+  .add("Default with label and disabled with checked", () => (
+    <Checkbox disabled checked>
+      Checkbox Disabled
+    </Checkbox>
   ))
+  .add("Small with children", () => <Checkbox size="small" />)
