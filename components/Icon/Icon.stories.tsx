9aedeab79b274aa3656d4fa3e265c786123958aa
Rohan Malhotra <rohan.root@gmail.com>
2019-05-31 15:14:17 +0530

    added fill prop in icon

--- components/Icon/Icon.stories.tsx
+++ components/Icon/Icon.stories.tsx
@@ -2,4 +2,6 @@ import React from "react"
 import { storiesOf } from "@storybook/react"
 import Icon from "./index"
 
-storiesOf("Icon", module).add("Basic Icon", () => <Icon type="oAddCircle" />)
+storiesOf("Icon", module)
+  .add("Basic Icon", () => <Icon type="oAddCircle" />)
+  .add("Icon with color", () => <Icon type="oAddCircle" fill="red" />)
