a11db171fcb5f6b1125547b9542b08e3b901bbcf
Anshuman Verma <anshu.av97@gmail.com>
2019-05-31 12:16:30 +0530

    rm some stories

--- components/Radio/Radio.stories.tsx
+++ components/Radio/Radio.stories.tsx
@@ -33,20 +33,3 @@ storiesOf("Radio", module)
     </RadioGroup>
   ))
   .add("With no child", () => <RadioGroup />)
-  .add("Default with checked", () => <Radio defaultChecked />)
-  .add("Small with default checked", () => <Radio size="small" />)
-  .add("Radio with label", () => <Radio>Radio</Radio>)
-  .add("Default with disabled", () => <Radio disabled />)
-  .add("Default with disabled but checked", () => <Radio disabled checked />)
-  .add("Default with label and disabled with checked", () => (
-    <Radio disabled checked>
-      Radio Disabled
-    </Radio>
-  ))
-  .add("With children", () => (
-    <RadioGroup>
-      <Radio value={1} size="small" />
-      <Radio value={2} size="small" />
-      <Radio value={3} size="small" />
-    </RadioGroup>
-  ))
