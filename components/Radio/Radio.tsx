37f3e179b75b2b00cc8633e3b35d57083ebddbcb
Anshuman Verma <anshu.av97@gmail.com>
2019-06-21 19:31:58 +0530

    Add some tests

--- components/Radio/Radio.tsx
+++ components/Radio/Radio.tsx
@@ -3,10 +3,15 @@ import RadioWrapper from "./RadioWrapper"
 import { RadioProps } from "./Interface"
 
 const Radio: SFC<RadioProps> = props => {
-  const { style, className, children, labelStyle, value } = props
+  const { style, className, children, labelStyle, value, ...otherProps } = props
   return (
     <label className={className} style={{ display: "flex", ...style }}>
-      <RadioWrapper prefixCls="knit-radio" value={value} {...props} />
+      <RadioWrapper
+        prefixCls="knit-radio"
+        value={value}
+        {...props}
+        {...otherProps}
+      />
       {children && <span style={labelStyle}>{children}</span>}
     </label>
   )
