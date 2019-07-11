4affee00ac79ec11e4df295731dca17ceea9c906
arorayash <arorayash.thisisus@gmail.com>
2019-07-11 10:25:52 +0530

    added custom fill + story for showing all icons

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,7 +1,27 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
 import Icon from "./index"
+import * as Icons from "./Icons"
+
+const allIcons = Object.values(Icons)
 
 storiesOf("Icon", module)
   .add("Basic Icon", () => <Icon type="oAddCircle" />)
   .add("Icon with color", () => <Icon type="oAddCircle" fill="red" />)
+  .add("All icons", () => {
+    const allIconsDOM = allIcons.map((i, index) => (
+      <div key={index} style={{ margin: 20 }}>
+        {i({})}
+      </div>
+    ))
+    return (
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexWrap: "wrap",
+        }}>
+        {allIconsDOM}
+      </div>
+    )
+  })
--- src/components/Icon/Icons.tsx
+++ src/components/Icon/Icons.tsx
@@ -6,7 +6,10 @@ type IconType = {
   fill?: string
 }
 
-/*Note - `o` is shorthand for `outline`/
+/*Notes
+ - `o` is shorthand for `outline`
+ - `customFill` class can be used to override the fill of an element inside svg
+*/
 
 /**Custom Icons**/
 
@@ -620,7 +623,7 @@ export const customOFormatColorFill = ({
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg">
     <path d="M16.5601 8.94L7.62006 0L6.21006 1.41L8.59006 3.79L3.44006 8.94C2.85006 9.53 2.85006 10.48 3.44006 11.06L8.94006 16.56C9.23006 16.85 9.62006 17 10.0001 17C10.3801 17 10.7701 16.85 11.0601 16.56L16.5601 11.06C17.1501 10.48 17.1501 9.53 16.5601 8.94V8.94ZM5.21006 10L10.0001 5.21L14.7901 10H5.21006ZM19.0001 11.5C19.0001 11.5 17.0001 13.67 17.0001 15C17.0001 16.1 17.9001 17 19.0001 17C20.1001 17 21.0001 16.1 21.0001 15C21.0001 13.67 19.0001 11.5 19.0001 11.5Z" />
-    <path d="M0 20H24V24H0V20Z" fill="#E7D9F2" />
+    <path d="M0 20H24V24H0V20Z" fill="#E7D9F2" className="customFill"/>
   </svg>
 )
 
@@ -635,8 +638,8 @@ export const customOFormatColorText = ({
     fill={fill}
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg">
-    <path d="M0 20H24V24H0V20Z" fill="#FFC700" />
-    <path d="M11 3L5.5 17H7.75L8.87 14H15.12L16.24 17H18.49L13 3H11ZM9.62 12L12 5.67L14.38 12H9.62V12Z" />
+    <path d="M0 20H24V24H0V20Z" fill="#FFC700" className="customFill" />
+    <path d="M11 3L5.5 17H7.75L8.87 14H15.12L16.24 17H18.49L13 3H11ZM9.62 12L12 5.67L14.38 12H9.62V12Z"  />
   </svg>
 )
 
