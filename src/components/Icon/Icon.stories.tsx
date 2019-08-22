c5583e5081a25b93adb37bcebbe8d9683fe81283
Yash <arorayash@Yashs-MacBook-Pro-3.local>
2019-08-22 10:41:48 +0530

    added knobs to icon story

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -2,16 +2,23 @@ import React from "react"
 import { storiesOf } from "@storybook/react"
 import Icon from "./index"
 import * as Icons from "./Icons"
+import { withKnobs, select } from "@storybook/addon-knobs"
 
+const stories = storiesOf("Icons", module)
+stories.addDecorator(withKnobs)
+
+const sizes = ["16px", "18px", "24px"]
 const allIcons = Object.values(Icons)
 
-storiesOf("Icon", module)
+stories
   .add("Basic Icon", () => <Icon type="oAddCircle" />)
   .add("Icon with color", () => <Icon type="oAddCircle" fill="red" />)
   .add("All icons", () => {
-    const allIconsDOM = allIcons.map((i, index) => (
-      <div key={index} style={{ margin: 20 }}>
-        {i({})}
+    const size = select("Size", sizes, "18px")
+    const props = { width: size, height: size }
+    const allIconsDOM = allIcons.map((icon, index) => (
+      <div key={index} style={{ margin: 20 }} role="icon-wrapper">
+        {icon(props)}
       </div>
     ))
     return (
