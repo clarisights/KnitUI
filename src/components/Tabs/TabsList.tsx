3141041e09c8b1d413a0be103c514011e0cb434c
Rohan Malhotra <rohan.root@gmail.com>
2019-12-23 16:35:09 +0530

    Refactoring and type fixes

---
+++ src/components/Tabs/TabsList.tsx
@@ -0,0 +1,52 @@
+import React, { ReactNode } from "react"
+import { SortableContainer } from "react-sortable-hoc"
+import { TabItem } from "./TabItem"
+import { activeTabFlagsInterface } from "./types"
+
+type TabsListProps = {
+  items: Array<ReactNode>
+  onChange: (activeKey: string) => void
+  activeKey: string
+  activeKeyIndex: number
+  activeTabFlags: activeTabFlagsInterface
+  itemRef: React.RefObject<HTMLDivElement>
+  activeNxtRef: React.RefObject<HTMLDivElement>
+  activePrevRef: React.RefObject<HTMLDivElement>
+}
+
+export const TabsList = SortableContainer(
+  ({
+    items,
+    onChange,
+    activeKey,
+    activeKeyIndex,
+    activeTabFlags,
+    itemRef,
+    activeNxtRef,
+    activePrevRef,
+  }: TabsListProps) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+        }}>
+        <div />
+        {items.map((value, index) => (
+          <TabItem
+            onChange={onChange}
+            key={`tab-${index}`}
+            index={index}
+            itemIndex={index}
+            value={value}
+            activeKey={activeKey}
+            activeKeyIndex={activeKeyIndex}
+            activeTabFlags={activeTabFlags}
+            itemRef={itemRef}
+            activeNxtRef={activeNxtRef}
+            activePrevRef={activePrevRef}
+          />
+        ))}
+      </div>
+    )
+  }
+)
