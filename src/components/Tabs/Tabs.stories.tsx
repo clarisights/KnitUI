319a6640d8837b6c3c54cc0dfd89c4de92df75b9
Prasook Jain <prasookjain@gmail.com>
2020-01-16 11:44:14 +0530

    added story for hideTabContent prop

--- src/components/Tabs/Tabs.stories.tsx
+++ src/components/Tabs/Tabs.stories.tsx
@@ -29,7 +29,7 @@ stories
           width: "800px",
           fontSize: 14,
         }}>
-        <Tabs onAddTab={() => console.log("prasooj")}>
+        <Tabs onAddTab={() => alert("callback for create add")}>
           <TabPanel tab="Tab 1" tabKey="tab-1">
             tab 1 content here
           </TabPanel>
@@ -94,7 +94,7 @@ stories
       </div>
     )
   })
-  .add("Tab Panel with react component", () => {
+  .add("Tab Panel hideTabContent", () => {
     return (
       <div
         style={{
@@ -103,22 +103,9 @@ stories
           width: "800px",
           fontSize: 14,
         }}>
-        <Tabs onAddTab={() => console.log("prasooj")}>
-          <TabPanel
-            tab={
-              <div style={{ height: "100%", width: "100px", display: "flex" }}>
-                tab 1{" "}
-                <Button
-                  icon="oFilterList"
-                  size="small"
-                  onClick={() => {
-                    alert("Hello")
-                  }}
-                />
-              </div>
-            }
-            tabKey="tab-1">
-            <div>tab 1 content here</div>
+        <Tabs onAddTab={() => alert("callback for create add")} hideTabContent>
+          <TabPanel tab="Tab 1" tabKey="tab-1">
+            tab 1 content here
           </TabPanel>
           <TabPanel tab="Tab 2" tabKey="tab-2">
             tab 2 content here
@@ -154,3 +141,61 @@ stories
       </div>
     )
   })
+  .add("Tab Panel with react component", () => (
+    <div
+      style={{
+        border: "1px solid lightgray",
+        height: "360px",
+        width: "800px",
+        fontSize: 14,
+      }}>
+      <Tabs onAddTab={() => alert("callback for create add")}>
+        <TabPanel
+          tab={
+            <div style={{ height: "100%", width: "100px", display: "flex" }}>
+              tab 1{" "}
+              <Button
+                icon="oFilterList"
+                size="small"
+                onClick={() => {
+                  alert("Clicked on inside button")
+                }}
+              />
+            </div>
+          }
+          tabKey="tab-1">
+          <div>tab 1 content here</div>
+        </TabPanel>
+        <TabPanel tab="Tab 2" tabKey="tab-2">
+          tab 2 content here
+        </TabPanel>
+        <TabPanel tab="Tab 3" tabKey="tab-3">
+          tab 3 content here
+        </TabPanel>
+        <TabPanel tab="Tab 32" tabKey="tab-35">
+          tab 3 content here
+        </TabPanel>
+        <TabPanel tab="Tab 42" tabKey="tab-45">
+          tab 4 content here
+        </TabPanel>
+        <TabPanel tab="Tab 52" tabKey="tab-55">
+          tab 5 content here
+        </TabPanel>
+        <TabPanel tab="Tab 12" tabKey="tab-16">
+          tab 1 content here
+        </TabPanel>
+        <TabPanel tab="Tab 22" tabKey="tab-26">
+          tab 2 content here
+        </TabPanel>
+        <TabPanel tab="Tab 32" tabKey="tab-36">
+          tab 3 content here
+        </TabPanel>
+        <TabPanel tab="Tab 42" tabKey="tab-46">
+          tab 4 content here
+        </TabPanel>
+        <TabPanel tab="Tab 52" tabKey="tab-56">
+          tab 5 content here
+        </TabPanel>
+      </Tabs>
+    </div>
+  ))
