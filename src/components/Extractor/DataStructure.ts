5bf12b1849d4aab1c5bcaa509144509d3ce0ac54
Anshuman Verma <anshu.av97@gmail.com>
2020-01-29 16:51:09 +0530

    fix: migrate package to ts here

---
+++ src/components/Extractor/DataStructure.ts
@@ -0,0 +1,40 @@
+import { NodeType } from "./types"
+
+export class TreeNode {
+  value: null
+  children: NodeType[]
+  constructor(value) {
+    this.value = value
+    this.children = []
+  }
+
+  addChild(node: NodeType) {
+    this.children.push(node)
+  }
+
+  setValue(val) {
+    this.value = val
+  }
+
+  clearChildren() {
+    this.children = []
+  }
+}
+
+export class EditorState {
+  root: NodeType
+
+  initRoot(node) {
+    this.root = node
+  }
+
+  buildExpression = (node: NodeType = this.root): string => {
+    let str = ""
+    if (node.value.type === "string") return node.value.data
+    node.children.forEach((child, idx) => {
+      str += this.buildExpression(child)
+      str += idx === node.children.length - 1 ? "" : ", "
+    })
+    return `${node.value.data.label} (${str})`
+  }
+}
---
+++ src/components/Extractor/DropDown.tsx
@@ -0,0 +1,165 @@
+import React, { useRef, useState, useEffect, RefObject } from "react"
+import Exp from "./Expression"
+import DownSelect from "./DownSelect"
+import { DropdownProps } from "./types"
+
+const Drop = (props: DropdownProps) => {
+  const {
+    placeholder,
+    EditorData,
+    node,
+    initialFocus = false,
+    options,
+    validationFn,
+    onChangeFn,
+    expressionRootClass,
+    expressionInputClass,
+  } = props
+
+  const [value, setValue] = useState<string>("")
+  const [exp, setExp] = useState<boolean>(false)
+  const dropRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (dropRef.current && initialFocus) {
+      dropRef.current.focus()
+    }
+  }, [dropRef.current])
+
+  const fnKeys: string[] = options
+    .filter(fn => fn.type === "function")
+    .map(fn => fn.key)
+
+  const handleValueChange = e => {
+    const val = e.target.value
+    // const newNode = new TreeNode(val)
+    node.setValue({ data: val, type: "string" })
+    setValue(val)
+    if (fnKeys.includes(val.toLowerCase())) {
+      setExp(true)
+      node.setValue({
+        data: options.find(f => f.key === val.toLowerCase()),
+        type: "fn",
+      }) // todo
+    }
+    if (onChangeFn) onChangeFn(EditorData)
+    // console.log(EditorData.buildExpression())
+  }
+
+  const getNextNode = () => {
+    let currElement
+    if (dropRef.current) {
+      currElement = dropRef.current.parentElement
+    }
+    // if the present element has a next sibling, directly switch to next
+    if (currElement.nextElementSibling)
+      currElement = currElement.nextElementSibling
+    // else keep going levels up till you find a next node
+    // else {
+    // 	currElement = currElement.parentNode
+    // }
+    // this is when you want to skip the top levels and only play in text fields
+    else {
+      while (!currElement.nextElementSibling)
+        currElement = currElement.parentElement
+      currElement = currElement.nextElementSibling
+    }
+    // while (rootClasses.includes(finalElement.dataset.type)) {
+    // 	finalElement = finalElement.firstElementChild
+    // }
+
+    if (currElement.dataset.type !== "expression-root")
+      currElement = currElement.firstElementChild
+
+    return currElement
+  }
+
+  const getPrevNode = () => {
+    let currElement
+    if (dropRef.current) {
+      currElement = dropRef.current.parentElement
+    }
+    // if the present element has a next sibling, directly switch to next
+    if (currElement.previousElementSibling)
+      currElement = currElement.previousElementSibling
+    // else {
+    // 	currElement = currElement.parentNode
+    // }
+    // else keep going levels up till you find a next node
+    else {
+      if (!currElement.previousElementSibling)
+        currElement = currElement.parentElement
+      // if (currElement.dataset.type === 'expression-root' && initialFocus)
+      // 	return currElement
+      // currElement = currElement.lastElementChild
+    }
+    // some transitions as per the element we arrive on
+    while (currElement.dataset.type === "expression-root" && !initialFocus) {
+      currElement = currElement.lastElementChild
+    }
+    if (currElement.dataset.type === "expression-input-root") {
+      currElement = currElement.firstElementChild
+    }
+    // console.log(currElement)
+    return currElement
+  }
+
+  const handleDir = e => {
+    e.stopPropagation()
+    const inputNode = dropRef.current!
+    switch (e.keyCode) {
+      case 39:
+        // only when I'm at last caret position
+        if (inputNode.selectionStart !== inputNode.value.length) return
+        e.preventDefault()
+        const nextNode = getNextNode()
+        if (nextNode) nextNode.focus()
+        return
+      case 37:
+        // only when at caret position is 0
+        if (inputNode.selectionStart !== 0) return
+        e.preventDefault()
+        const prevNode = getPrevNode()
+        if (prevNode) prevNode.focus()
+        return
+      default:
+        return
+    }
+  }
+
+  if (!exp) {
+    // this is an approximation
+    // let inputLen = `${(value
+    // 	? value.length
+    // 	: placeholder && placeholder.length) * 8}px`
+    // if (dropRef.current) inputLen = dropRef.current.scrollWidth
+    return (
+      <DownSelect
+        inputRef={dropRef}
+        inputPlaceholder={placeholder}
+        options={options}
+        onKeyDown={e => handleDir(e)}
+        inputValue={value}
+        validationFn={validationFn}
+        expressionInputClass={expressionInputClass}
+        handleValueChange={handleValueChange}
+      />
+    )
+  }
+  return (
+    <Exp
+      EditorData={EditorData}
+      options={options}
+      setExp={setExp}
+      setValue={setValue}
+      node={node}
+      onChangeFn={onChangeFn}
+      validationFn={validationFn}
+      expressionRootClass={expressionRootClass}
+      expressionInputClass={expressionInputClass}
+      fname={value}
+    />
+  )
+}
+
+export default Drop
