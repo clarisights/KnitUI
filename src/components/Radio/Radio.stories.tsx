c968a50e786613bddac3ee5ddfe2a57c19cfc792
Piyush Pawar <piyushpawar25@gmail.com>
2020-01-31 18:05:41 +0530

    Made Radio button's inner circle concentric with outer circle

--- src/components/Radio/Radio.stories.tsx
+++ src/components/Radio/Radio.stories.tsx
@@ -27,19 +27,19 @@ storiesOf("Radio", module)
   ))
   .add("Medium with 2 in group", () => (
     <RadioGroup size="medium">
-      <RadioGroup.Item value={1} />
-      <RadioGroup.Item value={2} />
+      <RadioGroup.Item value={1}>Option 1</RadioGroup.Item>
+      <RadioGroup.Item value={2}>Option 2</RadioGroup.Item>
     </RadioGroup>
   ))
   .add("Medium with 2 in group and with default value", () => (
     <RadioGroup size="medium" defaultValue={1}>
-      <RadioGroup.Item value={1} />
-      <RadioGroup.Item value={2} />
+      <RadioGroup.Item value={1}>Option 1</RadioGroup.Item>
+      <RadioGroup.Item value={2}>Option 2</RadioGroup.Item>
     </RadioGroup>
   ))
   .add("With single child", () => (
     <RadioGroup>
-      <RadioGroup.Item value={1} />
+      <RadioGroup.Item value={1}>Option 1</RadioGroup.Item>
     </RadioGroup>
   ))
   .add("With no child", () => <RadioGroup />)
