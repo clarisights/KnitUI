0532c09293ec9a5a79869e2ffabda866b2a0a371
Anshuman Verma <anshu.av97@gmail.com>
2020-02-18 23:02:58 +0530

    chore: update comments in link types

--- src/components/Link/types.ts
+++ src/components/Link/types.ts
@@ -2,10 +2,12 @@ import { ReactNode } from "react"
 import { BaseComponentProps } from "../../common/types"
 
 export interface ILinkProps extends BaseComponentProps {
-  /** This is a placeholder description */
   href: string
+  /** whether to show underline or not, default to true */
   underline?: boolean
   children: ReactNode
+  /** Disabled link, default to false */
   disabled?: boolean
+  /** Opens link in a new tab, default to false */
   newTab?: boolean
 }
