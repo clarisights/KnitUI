e50431c6abf0dd28965fb2dccb7a0fa75d1a27d3
Yash Arora <arorayash.work@gmail.com>
2020-03-09 11:23:47 +0530

    updated stories

--- src/components/Loaders/IconLoader.stories.tsx
+++ src/components/Loaders/IconLoader.stories.tsx
@@ -1,14 +1,11 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
-import { withKnobs, select } from "@storybook/addon-knobs"
 import IconLoader from "./index"
 const Readme = require("./README.md")
 
 storiesOf("Loaders", module)
-  .addDecorator(withKnobs)
   .addParameters({
     readme: {
-      // Show readme at the addons panel
       sidebar: Readme,
     },
   })
@@ -18,5 +15,13 @@ storiesOf("Loaders", module)
         width: 40,
       }}>
       <IconLoader size="1.8rem" />
+    </div>
+  ))
+  .add("icon loader, with custom color & strokeWidth", () => (
+    <div
+      style={{
+        width: 40,
+      }}>
+      <IconLoader size="2.8rem" strokeWidth={2} fill="#036600" />
     </div>
   ))
